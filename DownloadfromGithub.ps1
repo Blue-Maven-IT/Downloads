cef7532baa3f02c83ba8a9c60b9df92c024cfe89
bangiebindy <48794398+bangiebindy@users.noreply.github.com>
2024-07-12 09:21:28 -0400

    Add files via upload

---
+++ DownloadfromGithub.ps1
@@ -0,0 +1,11 @@
+﻿$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://github.com/Blue-Maven-IT/Downloads/raw/main/TAIAECCustomizationv1.zip")
+
+
+
+DO {write-host "downloading" }
+UNTIL (Test-Path -Path "C:\docs\TAIAECCustomizationv1.zip") {write-host "finished downloading; ready to expand archive"}
+
+Start-Sleep -Seconds 10
+
+Expand-Archive -LiteralPath 'C:\docs\TAIAECCustomizationv1.zip'
